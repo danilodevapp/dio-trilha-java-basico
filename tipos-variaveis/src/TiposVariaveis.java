a4eddcc99af6c834444fbf4c9a1fba8fc860af14
danilodevapp <danilodevapp@gmail.com>
2024-04-18 15:33:02 -0300

    Estudo sobre Tipos primitivos e variáveis imutáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Trabalhando com variáveis em Java!");
+        System.out.println("");
+
+        double salarioMinimo = 2500.00;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 2;
+
+        //O valor nunca poderá mudar quando a variável for declarada em "CAIXA ALTA".
+        //Ex.:
+
+        //Dessa mameira, ainda se premite alterar o valor
+        //double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 3.10;
+
+        //Para torná-la imutável usa-se o prefixo "final" antes da declaração, ex.:
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
